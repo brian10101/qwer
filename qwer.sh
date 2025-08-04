caae33e0da0037f59ed6384fa489e31b42f9530e
brian10101 <143109840+brian10101@users.noreply.github.com>
2025-08-04 19:00:16 -0400

    Updated qwer.sh to decrease verbosity and fix wording

--- qwer.sh
+++ qwer.sh
@@ -6,14 +6,14 @@ VERSION="3"
 
 case "$1" in
     --help) 
-        echo "qwer - A tool to clear terminal and return 'ls', the current time, and working directory all at once in a user firendly view" 
+        echo "qwer - A terminal tool to clear terminal and return 'ls', the current time, and working directory all at once in a user firendly view" 
         echo "Usage: qwer"
         echo ""
         echo "Options:"
         echo "  --help - Show this message"
         echo "  --version - Show version number"
         echo "Uninstall:"
-        echo "Navegate to the qwer directory"
+        echo "Navegate to qwer directory"
         echo "Run 'bash uninstall.sh'"
         exit 0
         ;;
@@ -24,16 +24,13 @@ case "$1" in
 esac                
 
 
-#Function to convert 24-hour time to 12-hour format without seconds
+#Function to convert 24-hour time to 12 hour format without seconds
 convert_tim() {
-    #Get current time in HH:MM:SS format
     full_time=$(date +"%T")
 
-    #Extract hour and minute
     hour=$(echo "$full_time" | cut -d ':' -f 1)
     minute=$(echo "$full_time" | cut -d ':' -f 2)
 
-    #Convert to 12-hour format
     if [ "$hour" -gt 12 ]; then
         hour=$((hour - 12))
     elif [ "$hour" -eq 0 ]; then
